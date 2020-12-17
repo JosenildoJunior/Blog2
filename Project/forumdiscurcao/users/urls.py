0bed561d45e2b84394d51e977b8a34e3328183d7
Josenildo junior <josenildojunior20155@gmail.com>
2020-12-17 00:47:00 -0300

    att 17/12 - 00:46 am

--- Project/forumdiscurcao/users/urls.py
+++ Project/forumdiscurcao/users/urls.py
@@ -7,5 +7,6 @@ app_name = 'users'
 urlpatterns = [
     # página inicial !
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
 ]
